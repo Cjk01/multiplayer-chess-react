276a79b2553b494dd0e1d7c1c1c2307f9f216bf1
Craig <craigkrikorian@gmail.com>
2020-12-26 03:40:28 -0500

    formatting changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
   <div>
     <App />
-    
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
--- src/server.js
+++ src/server.js
@@ -1,29 +1,24 @@
-
-
-const http = require('http');
-const express = require('express');
+const http = require("http");
+const express = require("express");
 
 const app = express(); // creating instance of express
-const server = http.createServer(app) // creating http server from express instance & enabling cross access origin resource sharing
+const server = http.createServer(app); // creating http server from express instance & enabling cross access origin resource sharing
 const io = require("socket.io")(server, {
-  cors: {  // this is required or else you will receive a CORS error, if you are using v3 of socket.io
-    origin: 'http://localhost:3000',
+  cors: {
+    // this is required or else you will receive a CORS error, if you are using v3 of socket.io
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
-    
-  }
+  },
 }); // creating socketio server side with express http server
-
 
 // server-side
 io.on("connection", (socket) => {
-  
   console.log(socket.id + " has connected"); // x8WIv7-mJelg7on_ALbx
-  
-  msgToClient = socket.id + " this is the server speaking"
-  socket.emit("Hello" , msgToClient);
-  
+
+  msgToClient = socket.id + " this is the server speaking";
+  socket.emit("Hello", msgToClient);
 });
 
 const PORT = 8080;
 
-server.listen(PORT , () => console.log(`Server running on port:${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port:${PORT}`));
