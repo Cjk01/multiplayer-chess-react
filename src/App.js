9b36554edc0dba2a87442b3a3c7f260f6a8ddf94
Craig <craigkrikorian@gmail.com>
2020-12-27 02:27:41 -0500

    added Chessboard component. sockets reconfigured

--- src/App.js
+++ src/App.js
@@ -1,98 +1,113 @@
 import { io } from "socket.io-client";
 import React from "react";
+import Chessboard from "chessboardjsx";
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      socket: "",
-      UserID: "",
-      OpponentID: "",
-      input: "",
-      sendTo: "",
-      messages: [],
+      inGame: false,
+      passwordCreationInput: "",
+      gameJoinInput: "",
+      password: "",
+      userSocket: "",
+      userSocketId: "",
+      opponentSocketId: "",
+      userColor: "",
+      opponentColor: "",
+      currentPositionFen: "",
+      userInfoMessage: "",
     };
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleInputSubmit = this.handleInputSubmit.bind(this);
-    this.handleSenderChange = this.handleSenderChange.bind(this);
+
+    this.handleCreationInput = this.handleCreationInput.bind(this);
+    this.handleJoinInput = this.handleJoinInput.bind(this);
+    this.handleCreationInputChange = this.handleCreationInputChange.bind(this);
+    this.handleJoinInputChange = this.handleJoinInputChange.bind(this);
   }
+
   componentDidMount() {
     this.connect();
   }
   connect() {
     let socketTemp = io("http://localhost:8080");
-
-    // client-side socket handlers
     socketTemp.on("connect", () => {
-      console.log(socketTemp.id); //Ex.  x8WIv7-mJelg7on_ALbx
-      this.setState({ socket: socketTemp, UserID: socketTemp.id });
-      console.log("UID: " + socketTemp.id);
-      socketTemp.on("Hello", (msg) => {
-        console.log(msg);
+      // when an opponent enters password and sends game request
+      this.setState({ userSocket: socketTemp, userSocketId: socketTemp.id });
+      socketTemp.on("gameSend", (joinObj) => {
+        console.log("message received from" + joinObj.senderId);
+        if (this.state.inGame === false && this.state.password !== "") {
+          console.log("message success from" + joinObj.senderId);
+          this.setState({ opponentSocketId: joinObj.senderId });
+          let newObj = {
+            usrId: this.state.userSocketId,
+            ownerId: joinObj.senderId,
+          };
+          socketTemp.emit("finalShake", newObj); // final handshake sent
+          this.setState({ inGame: true });
+        }
       });
-      // reads out private message
-      socketTemp.on(socketTemp.id, (receivedMsg) => {
-        console.log(
-          "message from" + receivedMsg.senderID + ": \n" + receivedMsg.message
-        );
-        let newmsg = receivedMsg.senderID + ": " + receivedMsg.message;
-        console.log("Thanks for the message, " + receivedMsg.senderID + "!");
-        this.setState({
-          messages: [...this.state.messages, newmsg],
-        });
+      socketTemp.on("NewCurrentPosition", (FENstring) => {
+        //updates the new current chess position
+        this.setState({ currentPositionFen: FENstring });
+      });
+      socketTemp.on(socketTemp.id, (oppID) => {
+        console.log("final shake ");
+        this.setState({ opponentSocketId: oppID }); // receives final handshake
+
+        this.setState({ inGame: true });
       });
     });
   }
-
-  handleInputChange(change) {
-    console.log(change.target.value);
-    this.setState({ input: change.target.value });
-  }
-  handleSenderChange(change) {
-    console.log(change.target.value);
-    this.setState({ sendTo: change.target.value });
-  }
-  handleInputSubmit() {
-    let submission = this.state.input;
-    //let sckt = this.state.socket;
-    let Uid = this.state.UserID;
-    let sendTo = this.state.sendTo;
-    console.log("submission " + submission);
-    let msgObj = {
-      recipientID: sendTo,
-      senderID: Uid,
-      messageContent: submission,
+  handleJoinInput() {
+    // sends the game join request
+    let joinObject = {
+      senderId: this.state.userSocketId,
+      pw: this.state.gameJoinInput,
     };
-    console.log(msgObj);
-    console.log(this.state.socket);
-    this.state.socket.emit("PrivateMessage", msgObj);
-    console.log("after private emit from client");
-    let newMsg = Uid + ": " + submission;
-    this.setState({
-      messages: [...this.state.messages, newMsg],
-      input: "",
-      sendTo: "",
-    });
-    console.log(this.state.messages);
+    this.state.userSocket.emit("JoinGame", joinObject);
+    this.setState({ gameJoinInput: "" });
   }
-  sendPrivateMessage(ev) {}
+  handleJoinInputChange(ev) {
+    console.log(ev.target.value);
+    this.setState({ gameJoinInput: ev.target.value });
+  }
+  handleCreationInput() {
+    this.setState({ password: this.state.passwordCreationInput });
+    this.setState({ passwordCreationInput: "" });
+  }
+  handleCreationInputChange(ev) {
+    console.log(ev.target.value);
+    this.setState({ passwordCreationInput: ev.target.value });
+  }
+
   render() {
-    const messageList = this.state.messages.map((i) => (
-      <li key={i + 1}>{i}</li>
-    ));
-    return (
-      <div>
-        <h1>User socket ID: {this.state.socket.id}</h1>
+    const inGame = this.state.inGame;
+    let UserMenu;
+    if (inGame === false) {
+      UserMenu = (
+        <div>
+          <h1>Enter your game password</h1>
+          <input
+            value={this.state.passwordCreationInput}
+            onChange={this.handleCreationInputChange}
+          ></input>
+          <button onClick={this.handleCreationInput}>create game</button>
+          <h1>join with password</h1>
+          <input
+            value={this.state.gameJoinInput}
+            onChange={this.handleJoinInputChange}
+          ></input>
 
-        <h1>enter sender ID</h1>
-        <input value={this.state.sendTo} onChange={this.handleSenderChange} />
-        <h1>enter message</h1>
-        <input value={this.state.input} onChange={this.handleInputChange} />
-
-        <button onClick={this.handleInputSubmit}>Send Message </button>
-
-        <ul>{messageList}</ul>
-      </div>
-    );
+          <button onClick={this.handleJoinInput}>join game</button>
+        </div>
+      );
+    } else {
+      UserMenu = (
+        <div>
+          <Chessboard position={this.state.currentPositionFen} />
+        </div>
+      );
+    }
+    return <div>{UserMenu}</div>;
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -3,45 +3,26 @@ const express = require("express");
 
 const app = express(); // creating instance of express
 const server = http.createServer(app); // creating http server from express instance & enabling cross access origin resource sharing
+const serverBaseURL = "http://localhost:8080";
+const URLfrontEnd = "http://localhost:3000"; // place react app url here
 const io = require("socket.io")(server, {
   cors: {
     // this is required or else you will receive a CORS error, if you are using v3 of socket.io
-    origin: "http://localhost:3000",
+    origin: URLfrontEnd,
     methods: ["GET", "POST"],
   },
 }); // creating socketio server side with express http server
-
-// server-side
 io.on("connection", (socket) => {
-  console.log(socket.id + " has connected"); // x8WIv7-mJelg7on_ALbx
-
-  msgToClient = socket.id + " this is the server speaking";
-  socket.emit("Hello", msgToClient);
-
-  socket.on("PrivateMessage", (messageObject) => {
-    console.log("private message received by server");
-    console.log(messageObject);
-    let privMsg = {
-      recipientID: messageObject.recipientID,
-      senderID: messageObject.senderID,
-      message: messageObject.messageContent,
-    };
-    console.log("sending to : " + privMsg.recipientID);
-    socket.broadcast.emit(privMsg.recipientID, privMsg);
+  console.log("a user connected");
+  socket.on("finalShake", (Object) => {
+    socket.broadcast.emit(Object.ownerId, Object.usrId);
+  });
+  socket.on("JoinGame", (joinObject) => {
+    console.log("JoinGame request received from " + joinObject.senderId);
+    console.log("password of room: " + joinObject.pw);
+    socket.broadcast.emit("gameSend", joinObject);
   });
 });
-// receives a PrivateMessage request with a message object attached
-/* io.on("PrivateMessage", (messageObject) => {
-  console.log("private message received by server");
-  let privMsg = {
-    recipientID: messageObject.recipientID,
-    senderID: messageObject.senderID,
-    message: messageObject.messageContent,
-  };
-
-  io.to(privMsg.recipientID).emit(privMsg.recipientID, privMsg);
-}); */
-
 const PORT = 8080;
 
 server.listen(PORT, () => console.log(`Server running on port:${PORT}`));
